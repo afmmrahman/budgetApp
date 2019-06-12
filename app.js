345c1c4cff088408be235fceb19dd0930b8b11e3
A F M Muzahidur Rahman <Tareq@As-MacBook-Pro.home>
2019-06-12 14:56:58 +0300

    updated

---
+++ app.js
@@ -0,0 +1,25 @@
+// 1. Data Module 
+
+var budgetController = (function(){
+    // some code
+})();
+
+
+
+
+
+// 2. UI Module
+
+var UIController = (function(){
+    // some code
+})();
+
+
+
+
+
+// Controller Module
+
+var controller = (function(){
+    // some code
+})();
