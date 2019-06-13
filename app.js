5a6d1949cc508213691698bfec7057fd6b2a8dd7
A F M Muzahidur Rahman <Tareq@As-MacBook-Pro.home>
2019-06-13 21:24:28 +0300

    updated

--- app.js
+++ app.js
@@ -75,7 +75,9 @@ var UIController = (function(){
         inputType: '.add__type',
         inputDescription: '.add__description',
         inputValue: '.add__value',
-        inputBtn: '.add__btn'
+        inputBtn: '.add__btn',
+        incomeContainer: '.income__list',
+        expensesContainer: '.expenses__list'
         
     };
     
@@ -88,6 +90,34 @@ var UIController = (function(){
                 description: document.querySelector(DOMStrings.inputDescription).value,
                 value: document.querySelector(DOMStrings.inputValue).value
             };
+        },
+        
+        addListItem: function(obj, type){
+            var html, newHtml, element;
+            
+            // Create HTML string with placeholder text
+            if(type === 'inc'){
+                element = DOMStrings.incomeContainer;
+                
+                html = '<div class="item clearfix" id="income-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+                
+            } else if (type === 'exp'){
+                element = DOMStrings.expensesContainer;
+                
+                html = '<div class="item clearfix" id="expense-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+            }
+            
+            
+            
+            
+            
+            // Replace the placeholder text with actual dada
+            newHtml = html.replace('%id%', obj.id);
+            newHtml = newHtml.replace('%description%', obj.description);
+            newHtml = newHtml.replace('%value%', obj.value);
+            
+            // Insert the HTML into the DOM
+            document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
         },
         
         getDOMstrings: function(){
@@ -135,6 +165,7 @@ var controller = (function(budgetCtrl, UICtrl){   // Module can also receive arg
         newItem = budgetCtrl.addItem(input.type, input.description, input.value);
         
         // 3. Add the item to the UI
+        UICtrl.addListItem(newItem, input.type);
         
         // 4. Calculate the budget
         
