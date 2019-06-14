d7d513d7ebea4aa45ae3254f543774456e80e343
A F M Muzahidur Rahman <Tareq@As-MacBook-Pro.home>
2019-06-15 01:46:29 +0300

    updated

--- app.js
+++ app.js
@@ -120,6 +120,21 @@ var UIController = (function(){
             document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
         },
         
+        clearFields: function(){
+            var fields, fieldsArr;
+            
+            fields = document.querySelectorAll(DOMStrings.inputDescription + ', ' + DOMStrings.inputValue);
+            
+            //Converting list into array
+            fieldsArr = Array.prototype.slice.call(fields);
+            
+            fieldsArr.forEach(function(current, index, array){
+                current.value = "";            
+            });
+            
+            fieldsArr[0].focus();
+        },
+        
         getDOMstrings: function(){
             return DOMStrings;
         }
@@ -167,9 +182,12 @@ var controller = (function(budgetCtrl, UICtrl){   // Module can also receive arg
         // 3. Add the item to the UI
         UICtrl.addListItem(newItem, input.type);
         
-        // 4. Calculate the budget
+        // 4. Clear the fields
+        UICtrl.clearFields();
         
-        // 5. Display the budget on the UI
+        // 5. Calculate the budget
+        
+        // 6. Display the budget on the UI
         
     };
     
