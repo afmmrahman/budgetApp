6916042b35d952689a20d1ae8d38403ff589d1a7
A F M Muzahidur Rahman <Tareq@As-MacBook-Pro.home>
2019-06-18 22:35:25 +0300

    updated

--- app.js
+++ app.js
@@ -6,6 +6,19 @@ var budgetController = (function(){
         this.id = id;
         this.description =  description;
         this.value = value;
+        this.percentage = -1;
+    };
+    
+    Expense.prototype.calcPercentage = function(totalIncome){
+        if(totalIncome > 0){
+            this.percentage = Math.round((this.value / totalIncome) * 100);
+        }else{
+            this.percentage = -1;
+        }     
+    };
+    
+    Expense.prototype.getPercentage = function(){
+        return this.percentage;
     };
     
     var Income = function(id, description, value){
@@ -96,6 +109,19 @@ var budgetController = (function(){
                 data.percentage = -1;
             }
             
+        },
+        
+        calculatePercentages: function(){
+            data.allItems.exp.forEach(function(cur){
+               cur.calcPercentage(data.totals.inc); 
+            });
+        },
+        
+        getPercentages: function(){
+            var allPerc = data.allItems.exp.map(function(cur){
+                return cur.getPercentage();
+            });
+            return allPerc;
         },
         
         getBudget: function(){
@@ -264,10 +290,13 @@ var controller = (function(budgetCtrl, UICtrl){   // Module can also receive arg
     var updatePercentages = function(){
       
         // 1. Calculate percentages
+        budgetCtrl.calculatePercentages();
         
         // 2. Read percentages from budget controller
+        var percentages = budgetCtrl.getPercentages();
         
         // 3. Update the UI with the new percentages
+        console.log(percentages );
     };
    
         
